769042353bfd337d67bb88209487c7c1a9635aab
Maaz Khurshid <maaz.khurshid60@gmail.com>
2026-02-23 17:08:40 +0500

    connect pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,6 +75,20 @@ export default function RootLayout({
             gtag('config', 'G-X86H4J4WQ4');
           `}
         </Script>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1435291848047613');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body
         suppressHydrationWarning
@@ -87,6 +101,14 @@ export default function RootLayout({
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
+        </noscript>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1435291848047613&ev=PageView&noscript=1"
+          />
         </noscript>
         <ScrollToTop />
         <Navbar />
