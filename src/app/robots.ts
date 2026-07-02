85abfe5b4cafeb78a196dbcab057414c0201c582
Maaz Khurshid <maaz.khurshid60@gmail.com>
2026-07-02 12:36:36 +0500

    crawling blocked

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,16 @@
 import { MetadataRoute } from "next";
+import { headers } from "next/headers";
 
-export default function robots(): MetadataRoute.Robots {
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  const headersList = await headers();
+  const host = headersList.get("host") ?? "";
+
+  if (host.includes("amplifyapp.com")) {
+    return {
+      rules: { userAgent: "*", disallow: "/" },
+    };
+  }
+
   return {
     rules: {
       userAgent: "*",
